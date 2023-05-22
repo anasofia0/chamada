2b67353279d751961e23f6d2bf48074ffdf854bf
anasofia0 <anasofialaranja@gmail.com>
2023-05-22 16:29:20 -0300

    honestamente eu to muito perdida

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__)
 
     basedir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(basedir, '..', 'chamada.db')
+    db_path = os.path.join(basedir, "..", "chamada.db")
     login_manager.init_app(app)
     app.config["SECRET_KEY"] = "env"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
@@ -23,10 +23,15 @@ def create_app():
     db.init_app(app)
     with app.app_context():
         from .models.user import User
+
         db.create_all()
     bootstrap.init_app(app)
 
     from .auth.auth import bp
+
+    app.register_blueprint(bp)
+    
+    from .controllers import bp
 
     app.register_blueprint(bp)
 
@@ -35,11 +40,14 @@ def create_app():
 
 app = create_app()
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @login_manager.user_loader
 def load_user(user_id):
     from .models.user import User
+
     return User.query.get(user_id)
